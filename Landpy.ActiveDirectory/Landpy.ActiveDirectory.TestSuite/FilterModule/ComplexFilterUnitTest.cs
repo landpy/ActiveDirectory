9390c1f4072ba5d8c3120f34fb9a0c2486e0e2b2
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2013-09-12 03:21:03 +0000

    Add ComplexFilterUnitTest.

---
+++ Landpy.ActiveDirectory/Landpy.ActiveDirectory.TestSuite/FilterModule/ComplexFilterUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Landpy.ActiveDirectory.Core.Filter;
+using Landpy.ActiveDirectory.Core.Filter.Expression;
+using Landpy.ActiveDirectory.Entity.Attribute.Name;
+using Landpy.ActiveDirectory.Entity.Object;
+using Landpy.ActiveDirectory.TestSuite.Common;
+using NUnit.Framework;
+using System;
+using Is = Landpy.ActiveDirectory.Core.Filter.Expression.Is;
+
+namespace Landpy.ActiveDirectory.TestSuite.FilterModule
+{
+    class ComplexFilterUnitTest : BaseUnitTest
+    {
+        [TestCase]
+        public void TestComplexFilter()
+        {
+            // Query condition:
+            // 1. Belongs to "pangxiaoliangOU" OU,CN start with "pang", AD object type is user.
+            // 2. Belongs to "pangxiaoliangOU" OU, CN end with "liu" and Mail is "mv@live.cn",  AD object type is user.
+            IFilter filter =
+                new And(
+                    new IsUser(),
+                    new Is(OrganizationalUnitAttributeNames.OU, "pangxiaoliangOU"),
+                    new Or(
+                            new StartWith(AttributeNames.CN, "pang"),
+                            new And(
+                                new EndWith(AttributeNames.CN, "liu"),
+                                new Is(PersonAttributeNames.Mail, "mv@live.cn")
+                                )
+                        )
+                    );
+            Assert.AreEqual("(&(objectClass=user)(ou=pangxiaoliangOU)(|(cn=pang*)(&(cn=*liu)(mail=mv@live.cn))))", filter.BuildFilter());
+            // Output the user object display name.
+            foreach (var userObject in UserObject.FindAll(this.ADOperator, filter))
+            {
+                using (userObject)
+                {
+                    Console.WriteLine(userObject.DisplayName);
+                }
+            }
+        }
+    }
+}
