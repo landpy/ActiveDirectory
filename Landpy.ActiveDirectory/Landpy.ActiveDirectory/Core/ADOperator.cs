c841e2c40c21c376e78591f1f616c20f0efcb71c
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2013-09-22 08:58:20 +0000

    Update ADOperator.

--- Landpy.ActiveDirectory/Landpy.ActiveDirectory/Core/ADOperator.cs
+++ Landpy.ActiveDirectory/Landpy.ActiveDirectory/Core/ADOperator.cs
@@ -1,21 +1,34 @@
 ﻿namespace Landpy.ActiveDirectory.Core
 {
-    class ADOperator : IADOperator
+    /// <summary>
+    /// The ActiveDirectory operator.
+    /// </summary>
+    public class ADOperator : IADOperator
     {
         private string UserLoginName { get; set; }
         private string Password { get; set; }
-        private string OperatorDomainName { get; set; }
+        private string OperateDomainName { get; set; }
 
-        public ADOperator(string userLoginName, string password, string operatorDomainName)
+        /// <summary>
+        /// The constructor with user login name, user password and name of domain which will be operated params.
+        /// </summary>
+        /// <param name="userLoginName">The user login name.</param>
+        /// <param name="password">The user password.</param>
+        /// <param name="operateDomainName">The name of domain which will be operated.</param>
+        public ADOperator(string userLoginName, string password, string operateDomainName)
         {
             this.UserLoginName = userLoginName;
             this.Password = password;
-            this.OperatorDomainName = operatorDomainName;
+            this.OperateDomainName = operateDomainName;
         }
 
+        /// <summary>
+        /// Get the ActiveDirectory operator info.
+        /// </summary>
+        /// <returns></returns>
         public ADOperatorInfo GetOperatorInfo()
         {
-            return new ADOperatorInfo { UserLoginName = this.UserLoginName, OperateDomainName = this.OperatorDomainName, Password = this.Password };
+            return new ADOperatorInfo { UserLoginName = this.UserLoginName, OperateDomainName = this.OperateDomainName, Password = this.Password };
         }
     }
 }
