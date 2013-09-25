cd0921a3521c342fe6f88a4df5583c47c01e62e0
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2013-09-25 07:50:32 +0000

    Add custom filter, you can use native AD filter string now.

---
+++ Landpy.ActiveDirectory/Landpy.ActiveDirectory.TestSuite/FilterModule/CustomFilterUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Landpy.ActiveDirectory.Core.Filter;
+using Landpy.ActiveDirectory.Core.Filter.Expression;
+using Landpy.ActiveDirectory.TestSuite.Common;
+using NUnit.Framework;
+
+namespace Landpy.ActiveDirectory.TestSuite.FilterModule
+{
+    class CustomFilterUnitTest : BaseUnitTest
+    {
+        private IFilter CustomFilter { get; set; }
+
+        protected override void SetUp()
+        {
+            this.CustomFilter = new Custom("(cn=pangxiaoliang)");
+        }
+
+        [TestCase]
+        public void TestCustomFilter()
+        {
+            Assert.AreEqual("(cn=pangxiaoliang)", this.CustomFilter.BuildFilter());
+        }
+    }
+}
