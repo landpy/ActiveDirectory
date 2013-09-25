e41492cf8508ac3042ea6695ddf73c4147dbbdd0
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2013-09-25 07:50:32 +0000

    Add custom filter, you can use native AD filter string now.

---
+++ Landpy.ActiveDirectory/Landpy.ActiveDirectory/Core/Filter/Expression/Custom.cs
@@ -0,0 +1,25 @@
+﻿namespace Landpy.ActiveDirectory.Core.Filter.Expression
+{
+    public class Custom : IFilter
+    {
+        private string FilterString { get; set; }
+
+        /// <summary>
+        ///  The AD custom filter, you can add native fitler string.
+        /// </summary>
+        /// <param name="filterString">The native filter string.</param>
+        public Custom(string filterString)
+        {
+            this.FilterString = filterString;
+        }
+
+        /// <summary>
+        /// Build the AD native filter string.
+        /// </summary>
+        /// <returns>The native filter string.</returns>
+        public string BuildFilter()
+        {
+            return this.FilterString;
+        }
+    }
+}
