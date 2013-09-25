33e7f0f7f9ad2fbd8c13cba63d39723616a6d0f4
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2013-09-25 07:57:15 +0000

    Update comments.

--- Landpy.ActiveDirectory/Landpy.ActiveDirectory/Core/Filter/Expression/Custom.cs
+++ Landpy.ActiveDirectory/Landpy.ActiveDirectory/Core/Filter/Expression/Custom.cs
@@ -1,11 +1,14 @@
 ﻿namespace Landpy.ActiveDirectory.Core.Filter.Expression
 {
+    /// <summary>
+    /// The AD custom filter, you can add native fitler string.
+    /// </summary>
     public class Custom : IFilter
     {
         private string FilterString { get; set; }
 
         /// <summary>
-        ///  The AD custom filter, you can add native fitler string.
+        ///  The constructure with native fitler string param.
         /// </summary>
         /// <param name="filterString">The native filter string.</param>
         public Custom(string filterString)
