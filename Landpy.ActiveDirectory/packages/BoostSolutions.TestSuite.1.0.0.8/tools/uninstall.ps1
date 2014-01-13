8bf0a1d4a9bec770dd7f4cdded29a2a097b6a791
pangxiaoliang_cp <SND\pangxiaoliang_cp>
2014-01-13 05:48:28 +0000

    Remove nuget package.

--- Landpy.ActiveDirectory/packages/BoostSolutions.TestSuite.1.0.0.8/tools/uninstall.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-$solution = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
-$schemaDirectoryPath = $solution.FullName.Substring(0, $solution.FullName.LastIndexOf('\')) +"\Schema"
-foreach ($project in $solution.Projects) { if($project.Name -eq "Schema"){$schemaProject = $project} }
-Remove-Item -Recurse -Force $schemaDirectoryPath
-$schemaProject.Delete()
